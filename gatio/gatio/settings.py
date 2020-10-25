fc66feb1b5c2e4de55b6e31ee07d17fc0d1a390d
Bahrami <baherami@gmail.com>
2020-10-25 12:30:37 +0200

    Revert "Fix security warning"
    This reverts commit 75f96307c577940fc21c9562bf8b4bd200ae7ced.

--- gatio/gatio/settings.py
+++ gatio/gatio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'uc^q&1t%%*qp6j6spc6*s0tb%1qg(-pqoc0(@o@1mh-ju&36by'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
