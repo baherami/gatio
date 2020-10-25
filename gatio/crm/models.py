1d01320156b60fbb526262d8d31e083581252a1c
Bahrami <baherami@gmail.com>
2020-10-25 14:33:28 +0200

    Add models and migration

--- gatio/crm/models.py
+++ gatio/crm/models.py
@@ -4,32 +4,59 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Customer(models.Model):
+class BaseUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    phone = models.CharField(max_length=32)
+    email = models.CharField(max_length=32)
+    ssn = models.CharField(max_length=32)
+    company_name = models.CharField(max_length=32)
+    company_id = models.CharField(max_length=32)
+    address = models.CharField(max_length=32)
 
 
-class ScreenOwner(Customer):
+class Seller(BaseUser):
     pass
 
 
-class Advertiser(Customer):
-    pass
+class ScreenOwner(BaseUser):
+    rate = models.IntegerField()
+    credit = models.IntegerField()
+
+
+class Advertiser(BaseUser):
+    rate = models.IntegerField()
+    credit = models.IntegerField()
 
 
 class Contract(models.Model):
-    pass
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    start = models.DateField()
+    end = models.DateField()
 
 class Match(models.Model):
-    pass
-    
-class Screen(models.Model):
-    code = models.CharField(max_length=30)
-
+    screen_owner = models.ForeignKey(ScreenOwner, on_delete=models.CASCADE)
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
+    start = models.DateField()
+    end = models.DateField()
+    frequency = models.IntegerField()
+    price = models.IntegerField()
 
 class Advertisement(models.Model):
-    pass
+    code = models.CharField(max_length=32)
+    media_url = models.URLField()
+    length = models.DurationField()
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
 
 
-class seller(models.Model):
-    pass
+class ScreenSpecifications(models.Model):
+    code = models.CharField(max_length=30)
+    size = models.IntegerField()
+
+
+class Screen(models.Model):
+    specification = models.ForeignKey(ScreenSpecifications, on_delete=models.CASCADE)
+    screenowner = models.ForeignKey(ScreenOwner, on_delete=models.CASCADE)
 
