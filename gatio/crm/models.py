8f4a9826d4001c8f0b54fc4eab7e04a96f310f65
Bahrami <baherami@gmail.com>
2020-10-25 12:51:50 +0200

    Add model structures

--- gatio/crm/models.py
+++ gatio/crm/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class ScreenOwner(Customer):
+    pass
+
+
+class Advertiser(Customer):
+    pass
+
+
+class Contract(models.Model):
+    pass
+
+class Match(models.Model):
+    pass
+    
+class Screen(models.Model):
+    code = models.CharField(max_length=30)
+
+
+class Advertisement(models.Model):
+    pass
+
+
+class seller(models.Model):
+    pass
+
